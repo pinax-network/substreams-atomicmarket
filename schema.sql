baabd447976778b8d414e4afef4455785c6a30c1
Pelotfr <polomeignan@hotmail.fr>
2023-10-27 10:57:14 +0200

    Swapped from FixedString(N) to String to avoid Clickhouse null bytes filling

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Sales
 (
     sale_id     UInt64,
-    trx_id      FixedString(64),
+    trx_id      String,
     asset_ids   Array(UInt64),
     listing_price_amount  Int64,
     listing_price_precision  UInt8,
-    listing_price_symcode    FixedString(7),
-    collection_name FixedString(12),
+    listing_price_symcode    String,
+    collection_name String,
 )
 ENGINE = ReplacingMergeTree
 PRIMARY KEY (sale_id)
